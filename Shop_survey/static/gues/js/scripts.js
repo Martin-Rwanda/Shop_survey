9fe57fade80658cd92a1f8d328d0d6f7fc43cccb
Martin-Rwanda <ntezijmartin27@gmail.com>
2025-02-17 02:16:28 +0200

    adding some js

--- Shop_survey/static/gues/js/scripts.js
+++ Shop_survey/static/gues/js/scripts.js
@@ -1,7 +1,38 @@
-/*!
-* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", function() {
+    const steps = document.querySelectorAll(".form-step");
+    const nextBtns = document.querySelectorAll(".next");
+    const prevBtns = document.querySelectorAll(".prev");
+    const progressBar = document.getElementById("progress-bar");
+
+    let currentStep = 0;
+
+    function updateForm() {
+        steps.forEach((step, index) => {
+            step.classList.toggle("active", index === currentStep);
+        });
+        progressBar.style.width = `${((currentStep + 1) / steps.length) * 100}%`;
+    }
+
+    nextBtns.forEach(button => {
+        button.addEventListener("click", () => {
+            if (currentStep < steps.length - 1) {
+                currentStep++;
+                updateForm();
+            }
+        });
+    });
+
+    prevBtns.forEach(button => {
+        button.addEventListener("click", () => {
+            if (currentStep > 0) {
+                currentStep--;
+                updateForm();
+            }
+        });
+    });
+
+    document.getElementById("surveyForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Thank you for your feedback!");
+    });
+});
